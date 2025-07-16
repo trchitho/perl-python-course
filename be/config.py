0338ab57746b3af17098f82b658cae2bf75f0cb7
Tho <tranchitho160704@gmail.com>
2025-07-17 00:28:16 +0700

    update config.py

--- be/config.py
+++ be/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Thien%40020603@localhost/user'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:IoyhASPHWtYBcNvcJyZBoedoctZKPSzn@hopper.proxy.rlwy.net:35432/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
